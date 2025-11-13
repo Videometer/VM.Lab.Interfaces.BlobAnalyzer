352aaa39eba1baf73a4c8f95b74a7898d7e17d2f
Henrik Enggaard Hansen <heh@videometer.com>
2025-11-13 14:48:15 +0100

    New state for bins being set

--- VM.Lab.Interfaces.BlobAnalyzer/BlobAnalyzerState.cs
+++ VM.Lab.Interfaces.BlobAnalyzer/BlobAnalyzerState.cs
@@ -1,45 +1,47 @@
-﻿namespace VM.Lab.Interfaces.BlobAnalyzer
+﻿namespace VM.Lab.Interfaces.BlobAnalyzer;
+
+/// <summary>Enumerator with autofeeder states</summary>
+public enum BlobAnalyzerState
 {
-	/// <summary>Enumerator with autofeeder states</summary>
-	public enum BlobAnalyzerState
-	{
-		/// <summary>Not ready / no recipe loaded yet.</summary>
-		None,
+	/// <summary>Not ready / no recipe loaded yet.</summary>
+	None,
 
-		/// <summary>A recipe is being loaded</summary>
-		LOADING_RECIPE,
+	/// <summary>A recipe is being loaded</summary>
+	LOADING_RECIPE,
 		
-		/// <summary>Idle / Ready to start</summary>
-		IDLE,
+	/// <summary>Bin ids are being set</summary>
+	SETTING_BINS,
+		
+	/// <summary>Idle / Ready to start</summary>
+	IDLE,
 
-		/// <summary>Starting a measurement</summary>
-		STARTING_MEASUREMENT,
+	/// <summary>Starting a measurement</summary>
+	STARTING_MEASUREMENT,
 		
-		/// <summary>Measuring</summary>
-		MEASURING,
+	/// <summary>Measuring</summary>
+	MEASURING,
 		
-		/// <summary>Resuming a measurement after being in stopped state </summary>
-		RESUMING_MEASUREMENT,
+	/// <summary>Resuming a measurement after being in stopped state </summary>
+	RESUMING_MEASUREMENT,
 
-		/// <summary>Stopping pipeline, waiting for data to be processed </summary>
-		STOPPING_PIPELINE,
+	/// <summary>Stopping pipeline, waiting for data to be processed </summary>
+	STOPPING_PIPELINE,
 		
-		/// <summary>Stopping pipeline, waiting for data to be processed, when done perform a flush </summary>
-		STOPPING_PIPELINE_FLUSH,
+	/// <summary>Stopping pipeline, waiting for data to be processed, when done perform a flush </summary>
+	STOPPING_PIPELINE_FLUSH,
 		
-		/// <summary>Device stopped, we can resume or finish the measurement</summary>
-		STOPPED,
+	/// <summary>Device stopped, we can resume or finish the measurement</summary>
+	STOPPED,
 
-		/// <summary>Finising and saving results</summary>
-		FINALIZING_MEASUREMENT,
+	/// <summary>Finishing and saving results</summary>
+	FINALIZING_MEASUREMENT,
 
-		/// <summary> Flusing, go to IDLE after</summary>
-		FLUSHING_IDLE,
+	/// <summary> Flushing, go to IDLE after</summary>
+	FLUSHING_IDLE,
 		
-		/// <summary> Flusing, go to NONE after</summary>
-		FLUSHING_NONE,
+	/// <summary> Flushing, go to NONE after</summary>
+	FLUSHING_NONE,
 		
-		/// <summary> Flusing, go to STOPPED after</summary>
-		FLUSHING_STOPPED
-	}
+	/// <summary> Flushing, go to STOPPED after</summary>
+	FLUSHING_STOPPED
 }
